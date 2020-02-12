0e8e5e17270672483094848bef4dd3595b0b02a4
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-02-12 20:18:43 +0100

    Module 6: Add counter to cart in nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,41 +4,49 @@ import NavStyles from './styles/NavStyles';
 import User from './User';
 import { TOGGLE_CART_MUTATION } from '../mutations';
 import Signout from './Signout';
+import CartCount from './CartCount';
 
 const Nav = () => (
   <User>
-    {({ data: { me } }) => (
-      <NavStyles>
-        <Link href="/items">
-          <a>Items</a>
-        </Link>
-        {me ? (
-          <>
-            <Link href="/sell">
-              <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-              <a>Orders</a>
-            </Link>
-            <Link href="/me">
-              <a>Account</a>
-            </Link>
-            <Signout />
-            <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => (
-                <button type="button" onClick={toggleCart}>
-                  Cart
-                </button>
-              )}
-            </Mutation>
-          </>
-        ) : (
-          <Link href="/signup">
-            <a>Sign in</a>
+    {({ data: { me } }) => {
+      const totalItemsCount = me.cart.reduce(
+        (tally, cartItem) => tally + cartItem.quantity,
+        0
+      );
+      return (
+        <NavStyles>
+          <Link href="/items">
+            <a>Items</a>
           </Link>
-        )}
-      </NavStyles>
-    )}
+          {me ? (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+              <Signout />
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => (
+                  <button type="button" onClick={toggleCart}>
+                    Cart
+                    <CartCount count={totalItemsCount} />
+                  </button>
+                )}
+              </Mutation>
+            </>
+          ) : (
+            <Link href="/signup">
+              <a>Sign in</a>
+            </Link>
+          )}
+        </NavStyles>
+      );
+    }}
   </User>
 );
 
