249777c4f7d7923204589f228a9e813ec0f7f018
Nicola Ferracin <nicola.ferracin@gmail.com>
2020-02-28 16:14:50 +0100

    Module 8: Frontend - Create an order after getting the stripe token

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -6,12 +6,16 @@ import NProgress from 'nprogress';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import ErrorMessage from './ErrorMessage';
 import User from './User';
+import { CURRENT_USER_QUERY } from '../queries';
+import { CREATE_ORDER_MUTATION } from '../mutations';
 
 class TakeMyMoney extends Component {
   totalItems = cart => cart.reduce((acc, item) => acc + item.quantity, 0);
 
-  onToken = res => {
-    console.log(res);
+  onToken = async (res, createOrder) => {
+    const order = await createOrder({
+      variables: { token: res.id },
+    }).catch(err => alert(err.message));
   };
 
   render() {
@@ -19,18 +23,27 @@ class TakeMyMoney extends Component {
     return (
       <User>
         {({ data: { me } }) => (
-          <StripeCheckout
-            amount={calcTotalPrice(me.cart)}
-            name="SickFits"
-            description={`Order of ${this.totalItems(me.cart)} items`}
-            image={me.cart[0].item && me.cart[0].item.image}
-            stripeKey="pk_test_IqUWixldAKmVY25FalQjIQhw00ucxjsivn"
-            currency="EUR"
-            email={me.email}
-            token={res => this.onToken(res)}
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
           >
-            {children}
-          </StripeCheckout>
+            {createOrder => (
+              <StripeCheckout
+                amount={calcTotalPrice(me.cart)}
+                name="SickFits"
+                description={`Order of ${this.totalItems(me.cart)} items`}
+                image={
+                  me.cart.length && me.cart[0].item && me.cart[0].item.image
+                }
+                stripeKey="pk_test_IqUWixldAKmVY25FalQjIQhw00ucxjsivn"
+                currency="EUR"
+                email={me.email}
+                token={res => this.onToken(res, createOrder)}
+              >
+                {children}
+              </StripeCheckout>
+            )}
+          </Mutation>
         )}
       </User>
     );
